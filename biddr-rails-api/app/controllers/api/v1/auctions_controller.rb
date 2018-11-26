cd52ccc9892309e939aab41b5c25c3c25619f97d
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-11-26 09:55:11 -0800

    Back end creates new auction and renders json

--- biddr-rails-api/app/controllers/api/v1/auctions_controller.rb
+++ biddr-rails-api/app/controllers/api/v1/auctions_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::AuctionsController < ApplicationController
+  def create
+    auction = Auction.new auction_params
+    auction.save
+    render json:auction  
+  end
+
+  private
+  def auction_params
+    params.require(:auction).permit(:title, :details, :end_date, :reserve_price)
+  end
 end
