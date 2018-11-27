4dc5128f6379390ba032a21452f4ee33520f4d1e
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-11-26 16:56:48 -0800

    Auction Show page displays bids

--- biddr-rails-api/config/routes.rb
+++ biddr-rails-api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :auctions, only: [:create, :index, :show] do
+      resources :auctions do
         resources :bids, only: [:create]  
       end
 
--- biddr-react/src/components/ShowAuctionPage.js
+++ biddr-react/src/components/ShowAuctionPage.js
@@ -29,9 +29,16 @@ class ShowAuctionPage extends Component {
     return (
       <main className="ShowAuctionPage">
         <h2># {this.state.auction.id} {this.state.auction.title}</h2>
-        <h3>${this.state.auction.reserve_price} reserve price</h3>
-        <p>{this.state.auction.details}</p>
-        <em><p>Bidding will close on {this.state.auction.end_date}</p></em>
+        <div className="AuctionInfo">
+          <div className="Auction">
+            <h3>${this.state.auction.reserve_price} reserve price</h3>
+            <p>{this.state.auction.details}</p>
+            <em><p>Bidding will close on {this.state.auction.end_date}</p></em>
+          </div>
+          <div className="Bids">
+            <h2>Bids</h2>
+          </div>
+        </div>
       </main>
     );
   }
